36cc9564f42624d62a46f317c10c458ec41f2c29
Dr34mChas3r <gjkdsgkdfk@gmail.com>
2024-10-20 19:56:49 +0300

    k

--- Lab10cpp.cpp
+++ Lab10cpp.cpp
@@ -5,9 +5,23 @@
 using namespace std;
 
 int main() {
+
 	ifstream infile("input.txt");
 	if (!infile) {
-		cerr << "Oops! File not found" << endl;
+		cerr << "Oops! input file not found" << endl;
 		return 1;
 	}
+
+	ofstream outfile("output.txt");
+	if (!outfile) {
+		cerr << "Oops! output file not found" << endl;
+		return 1;
+	}
+
+	string line;
+	while (getline(infile, line)) {
+		cout << line << endl;
+	}
+	infile.close();
+	return 0;
 }
